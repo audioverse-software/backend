865774b36b321509c1776e24e828d167bb93db86
blurbeast <leumasre@gmail.com>
2025-06-02 19:35:52 +0100

    interest controller done

---
+++ src/routes/api/interest.api.ts
@@ -0,0 +1,7 @@
+import { InterestController } from "@/controllers/interests/interest.controller";
+import { Router } from "express";
+
+
+const router = Router();
+const interestController = new InterestController();
+
