1ac7c9cbf0cbe1a8b15caca30a4fbcb04a9ac022
blurbeast <leumasre@gmail.com>
2025-06-08 06:08:37 +0100

    done with the notification controller

--- src/controllers/notification/notification.controller.ts
+++ src/controllers/notification/notification.controller.ts
@@ -1,4 +1,4 @@
-import { CreateNotificationDTO } from "@/dtos/notification/notification.dto";
+import { CreateNotificationDTO, UpdateNotification } from "@/dtos/notification/notification.dto";
 import { NotificationService } from "@/services/notification/notification.service";
 import { Request, Response } from "express";
 
@@ -13,8 +13,77 @@ export class NotificationController {
 
 
     async createNotification(req: Request, res: Response) {
-        const createNotification: CreateNotificationDTO = req.body;
-        const response = this.notificationService.createNotification(createNotification);
-        res.json(response);
+        try{
+            const createNotification: CreateNotificationDTO = req.body;
+            const response = await this.notificationService.createNotification(createNotification);
+            res.status(201).json(response);
+
+        }catch(error) {
+            res.status(401).json({error});
+        }
+    }
+
+    async getASpecificUserNotification(req: Request, res: Response) {
+        try{
+            const notificationId: number = parseInt(req.params.id);
+            const userId: number = parseInt(req.params.userId);
+
+            const response = await this.notificationService.getNotificationById(notificationId, userId);
+
+            res.status(200).json(response);
+        }catch(error) {
+            res.status(403).json({error})
+        }
+    }
+
+    async getUserNotification(req: Request, res: Response) {
+
+        try{
+            const userId: number = parseInt(req.params.userId);
+            const status: any = req.query.status;
+
+            const response = await this.notificationService.getUserNotication(userId, status);
+
+            res.status(200).json(response);
+        }catch(error){
+            res.status(401).json({error});
+        }
+    }
+
+    async updateNotification(req: Request, res: Response) {
+        try{
+            const updateNotificationDto: UpdateNotification = req.body;
+            const response = await this.notificationService.updateNotication(updateNotificationDto);
+
+            res.status(200).json(response);
+        }catch(error) {
+            res.status(403).json({error})
+        }
+    }
+
+    async deleteNotification(req: Request, res: Response) {
+        try{
+            const id: number = parseInt(req.params.id);
+            const userId: number = parseInt(req.params.userId);
+
+            const response = await this.notificationService.deleteNotification(id, userId);
+
+            res.status(200).json(response);
+        }catch(error) {
+            res.status(401).json({error});
+        }
+    }
+
+    async deleteNotifications(req: Request, res: Response) {
+        try{
+            const userId: number = parseInt(req.params.userId);
+            const status: any = req.query.status;
+
+            const response = await this.notificationService.deleteNotifications(userId, status);
+
+            res.status(200).json(response);
+        }catch(error) {
+            res.status(402).json({error});
+        }
     }
 }
